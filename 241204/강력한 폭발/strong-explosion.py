bdaffde14ffe826c4ef73b5d0596ff07414e9a2b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-04 19:16:03 +0900

    Title: 강력한 폭발, Time: 51ms, Memory: 17MB, Status: Passed - CodeTree

--- 241204/강력한 폭발/strong-explosion.py
+++ 241204/강력한 폭발/strong-explosion.py
@@ -5,55 +5,56 @@
 '''
 n = int(input())
 
-graph = []
-bomb_position = []
-
+graph = []; bomb_position = []
 c = 0 # 폭탄 설치가 가능한 영역의 수
+visited = [[False]*n for _ in range(n)] # 각 영역에 폭탄이 터졌는지 안터졌는지 확인용
 for i in range(n):
     row = list(map(int,input().split()))
     for j in range(n):
         if row[j] == 1: # 폭탄을 놓을 수 있는 위치라면 기록
             bomb_position.append((i,j))
+            visited[i][j] = True # 폭탄 설치 가능 영역은 항상 터짐
             c+=1
     graph.append(row)
 
 bomb_type = [] # 각 위치에 설치할 폭탄의 유형
+
+result = 0; count = c # 폭탄 설치 가능 위치만큼
 # 어떤 폭탄을 설치할지 선정
 def dfs(cnt):
-    if cnt == n: # 10개 모두 설치 시
-        count_distroied_position() # 폭파된 영역 카운트
+    global result,count,visited
+    if cnt == c: # c개 모두 설치시
+        # 현재까지 폭파된 영역 갱신
+        result = max(result,count)
         return
     
     # 1,2,3 3가지 유형 중 1개의 폭탄 설치
     for i in range(1,4):
         bomb_type.append(i)
+        backup = [v[:] for v in visited]
+        # 폭탄 유형에 맞춰 폭탄 터뜨리기
+        destroy_count = destroy_bomb(cnt)
+        count += destroy_count
         dfs(cnt+1)
+        count -= destroy_count
+        visited = [b[:] for b in backup] # 백트래킹
         bomb_type.pop()
 
-# 해당 자리에 폭탄을 둠으로서 몇개의 폭탄이 터졌을지 카운트
-result = 0
+# 폭탄 터뜨리기
 # 폭탄 유형1, 2, 3
 dx = [[],[-1,-2,1,2], [-1,1,0,0], [-1,-1,1,1]]
 dy = [[],[0,0,0,0], [0,0,-1,1], [-1,1,-1,1]]
-def count_distroied_position():
-    global result
-    visited = [[False]*n for _ in range(n)] # 각 영역에 폭탄이 터졌는지 안터졌는지 확인용
-    
-    count = 0 # 터진 영역의 개수
-    for t in range(c): # c개의 폭탄 설치 예정
-        type = bomb_type[t] # i위치에 설치된 폭탄의 유형
-        x,y = bomb_position[t] # i번째 위치 초기 좌표
-        if not visited[x][y]:
-            visited[x][y] = True
-            count += 1
-        for i in range(len(dx[type])):
-            nx,ny = x + dx[type][i], y + dy[type][i]
-            # 영역을 벗어나지 않으면서 아직 터지지 않은 공간인 경우
-            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
-                count += 1
-                visited[nx][ny] = True
-    
-    result = max(result,count) # 더 많이 터진 경우로 정답 갱신
+def destroy_bomb(cnt):
+    type = bomb_type[cnt]
+    x, y = bomb_position[cnt]
+    temp_count = 0
+    for i in range(len(dx[type])):
+        nx,ny = x + dx[type][i], y + dy[type][i]
+        # 영역을 벗어나지 않으면서 아직 터지지 않은 공간인 경우
+        if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+            temp_count += 1
+            visited[nx][ny] = True
+    return temp_count
 
 dfs(0)
 print(result)
