4ed5d035ab998356c142ff10daadb2f77debfef1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-10 20:06:21 +0900

    Title: 대폭발, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241210/대폭발/big-explosion.py
+++ 241210/대폭발/big-explosion.py
@@ -1,31 +1,31 @@
 '''
 구현,시뮬레이션
 '''
+from collections import deque
 n,m,r,c = map(int,input().split())
 
-visited = [[False]*n for _ in range(n)] # 격자판 생성
+graph = [[0]*n for _ in range(n)] # 격자판 생성
+# 0인 좌표는 폭탄이 존재하지 않음
+queue = deque()
+queue.append((r-1,c-1))
 
 def simulation():
     dsx = [0,0,-1,1]; dsy = [-1,1,0,0]
     count = 1 # 폭탄의 개수 카운팅
-    time = 0
-    visited[r-1][c-1] = True
-    bomb_ps = [(r-1,c-1)]
-    while True:
-        time += 1
-        if time == m+1:
+    while queue:
+        x,y = queue.popleft()
+        graph[x][y] += 1 # 현재 폭탄이 있던 위치에서 1초가 지남
+        time = graph[x][y]
+        if time == m+1: # 목표 시간 도달시 종료
             return count
-        new_bomb_ps = []
         d = 2**(time-1)
-        for x,y in bomb_ps:
-            for dx,dy in zip(dsx,dsy):
-                nx = x + d*dx
-                ny = y + d*dy
-                if 0<=nx<n and 0<=ny<n and not visited[nx][ny]: # 폭탄이 놓인적 없는 위치로만 폭탄 이동 가능
-                    visited[nx][ny] = True
-                    new_bomb_ps.append((nx,ny))
-                    count+=1
-        new_bomb_ps.append((x,y)) # 현재 위치도 또한번 폭탄이 터지는 위치에 해당
-        bomb_ps = new_bomb_ps
-        
+        for dx,dy in zip(dsx,dsy):
+            nx = x + d*dx
+            ny = y + d*dy
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 0: # 폭탄이 놓인적 없는 위치로만 폭탄 이동 가능
+                graph[nx][ny] = graph[x][y]
+                queue.append((nx,ny))
+                count+=1
+        queue.append((x,y)) # 현재 위치도 또한번 폭탄이 터지는 위치에 해당
+
 print(simulation())
