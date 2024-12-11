5e99af60fa011149ef24d859e07542606a149bbe
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-11 17:05:46 +0900

    Title: 합쳐지는 구슬들, Time: 75ms, Memory: 17MB, Status: Passed - CodeTree

--- 241211/합쳐지는 구슬들/merge-marbles.py
+++ 241211/합쳐지는 구슬들/merge-marbles.py
@@ -1,94 +1,121 @@
-'''
-구현, 시뮬레이션
-'''
-n,m,t = map(int,input().split())
-'''
- ‘U', ‘D’, ‘R’, 'L’ 
- 위,아래,오른쪽,왼쪽
-'''
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
-mapper = {'U':0,'D':1,'R':2,'L':3}
+EMPTY = (0, 0, 0)
 
-beads = []
+# 변수 선언 및 입력:
+n, m, t = tuple(map(int, input().split()))
+grid = [
+    [EMPTY for _ in range(n)]
+    for _ in range(n)
+]
+next_grid = [
+    [EMPTY for _ in range(n)]
+    for _ in range(n)
+]
 
-# 구슬 정보 입력받기
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def next_pos(x, y, move_dir):
+    # 방향 전환을 쉽게 하기 위해 
+    # dx, dy 테크닉에서 0<->3, 1<->2가
+    # 서로 반대 방향이 되도록 정의합니다.
+    dxs, dys = [-1, 0, 0, 1], [0, 1, -1, 0]
+    
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+    
+    # 벽에 부딪히게 된다면, 방향만 전환해줍니다.
+    if not in_range(nx, ny):
+        move_dir = 3 - move_dir
+    # 그렇지 않다면, 한 칸 전진합니다.
+    else:
+        x, y = nx, ny
+    
+    return (x, y, move_dir)
+
+
+# (x, y) 위치에 새로운 구슬이 들어왔을 때 갱신을 진행합니다.
+def update(x, y, new_marble):
+    # 기존 구슬 정보입니다.
+    num, weight, move_dir = next_grid[x][y]
+    
+    # 새롭게 들어온 구슬 정보입니다.
+    new_num, new_weight, new_dir = new_marble
+    
+    # 새로 들어온 구슬이 더 우선순위가 높다면
+    # 번호와 방향은 새로운 구슬을 따르게 됩니다.
+    if new_num > num: 
+        next_grid[x][y] = (new_num, weight + new_weight, new_dir)
+    # 기존 구슬의 우선순위가 더 높다면
+    # 무게만 더해집니다.
+    else:
+        next_grid[x][y] = (num, weight + new_weight, move_dir)
+
+
+def move(x, y):
+    num, weight, move_dir = grid[x][y]
+    
+    # Step1. 현재 구슬의 다음 위치와 방향을 구합니다.
+    nx, ny, next_dir = next_pos(x, y, move_dir)
+    
+    # Step2. 구슬을 옮겨줍니다.
+    update(nx, ny, (num, weight, next_dir))
+
+
+def simulate():
+    # Step1. next_grid를 초기화합니다.
+    for i in range(n):
+        for j in range(n):
+            next_grid[i][j] = EMPTY
+    
+    # Step2. 각 구슬들을 한 칸씩 움직여줍니다.
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] != EMPTY:
+                move(i, j)
+
+    # Step3. next_grid 값을 grid로 옮겨줍니다.
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = next_grid[i][j]
+
+
+def get_marble_num():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] != EMPTY:
+                cnt += 1
+
+    return cnt
+
+
+def get_max_weight():
+    max_weight = 0
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] != EMPTY:
+                _, weight, _ = grid[i][j]
+                max_weight = max(max_weight, weight)
+
+    return max_weight
+
+	
+dir_mapper = {
+    "U": 0,
+    "R": 1,
+    "L": 2,
+    "D": 3
+}
+
 for i in range(m):
-    r,c,d,w = input().split()
-    # 구슬 정보 저장
-    beads.append([int(r)-1,int(c)-1,mapper[d],int(w),i]) # 가장 마지막 요소는 번호
+    r, c, d, w = tuple(input().split())
+    r, c, w = tuple(map(int, [r, c, w]))
+    grid[r - 1][c - 1] = (i + 1, w, dir_mapper[d])
 
-def in_range(x,y):
-    if 0<=x<n and 0<=y<n:
-        return True
-    return False
+# t초에 걸쳐 시뮬레이션을 진행합니다.
+for _ in range(t):
+    simulate()
 
-def reverse_dir(d):
-    if d == 0:
-        d = 1
-    elif d == 1:
-        d = 0
-    elif d == 2:
-        d = 3
-    elif d == 3:
-        d = 2
-    return d
-
-# 1. 구슬이 움직인다.
-def move_beads():
-    global beads
-    new_beads = [] # 새로 변화된 구슬의 위치
-    # 모든 구슬에 대하여
-    for x,y,d,w,num in beads:
-        nx = x + dx[d]
-        ny = y + dy[d]
-        # 영역을 벗어나는 경우라면 방향은 정 반대가 된다. (위치는 변화하지 않음)
-        if not in_range(nx,ny):
-            d = reverse_dir(d)
-            new_beads.append([x,y,d,w,num]) 
-            continue # 다른 구슬 탐색 - 현재 구슬 움직임 종료
-        # 영역을 벗어나지 않는다면 새로운 위치로 구슬 삽입
-        new_beads.append([nx,ny,d,w,num])
-    # 구슬 위치 정보 업데이트
-    beads = new_beads
-
-
-# 2. 같은 위치에 있는 구슬끼리 합쳐진다.
-def merge_beads():
-    global beads
-    is_checked = [False]*len(beads) # 각 구슬을 확인했는지 여부
-    new_beads = []
-    # 모든 구슬에 대하여
-    for i in range(len(beads)):
-        # i 번째 이후 구슬들에 대하여 i번째 구슬과 위치가 겹치는지 확인
-        x_i,y_i,d_i,w_i,num_i = beads[i]
-        if is_checked[i]: # 앞서 확인 처리된 구슬이라면 넘어가기
-            continue
-        for j in range(i+1,len(beads)):
-            x_j,y_j,d_j,w_j,num_j = beads[j]
-            if (x_i,y_i) == (x_j,y_j): # 확인 검사 필요?
-                is_checked[j] = True # 확인 처리
-                # 방향은 합쳐진 구슬들 중 가장 큰 번호가 매겨져있는 구슬의 방향을 따르게 되고
-                # 번호는 마찬가지로 충돌이 일어난 구슬들 중 가장 큰 번호를 갖게 된다.
-                if num_i < num_j:
-                    d_i = d_j 
-                    num_i = num_j
-                # 구슬의 무게는 해당 위치에 모인 모든 구슬의 합으로 결
-                w_i += w_j
-        new_beads.append([x_i,y_i,d_i,w_i,num_i])
-    beads = new_beads
-                   
-
-# t초 동안 시뮬레이션 수행
-for i in range(t):
-    # 1. 구슬이 움직인다.
-    move_beads()
-    # 2. 같은 위치에 존재하는 구슬끼리 합쳐진다.
-    merge_beads()
-
-# 남아있는 구슬의 수와 가장 무거운 구슬의 무게 출력
-max_weight = 1
-for x,y,d,w,num in beads:
-    max_weight = max(max_weight,w)
-
-print(len(beads),max_weight)
+marble_num, max_weight = get_marble_num(), get_max_weight()
+print(marble_num, max_weight)
