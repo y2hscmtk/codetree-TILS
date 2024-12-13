16c9d456a43f09a16ff85e00be7095ade364e86d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-14 02:58:50 +0900

    Title: 기울어진 직사각형의 회전, Time: 79ms, Memory: 18MB, Status: Passed - CodeTree

--- 241214/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 241214/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,88 +1,51 @@
+CCW = 0
+CW = 1
+
+# 변수 선언 및 입력:
 n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+temp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
-grid = [list(map(int,input().split())) for _ in range(n)]
 
-r,c,m1,m2,m3,m4,d = map(int,input().split())
-
-# d 
-# 0 : 반시계
-# 1 : 시계 방향
-
-r,c = r-1,c-1
-
-if d == 0:
-    # 방향 1
-    # r-1,c+1
-    first = grid[r-m1][c+m1]
-    i = 0
-    for i in range(m1):
-        grid[r-m1+i][c+m1-i] = grid[r-m1+i+1][c+m1-i-1]
-
-    # 방향 2
-    # r-1,c-1
-    x,y = r-m1, c+m1
-    second = grid[x-m2][y-m2]
-    j = 0
-    for i in range(m2-1):
-        grid[x-m2+i][y-m2+i] = grid[x-m2+i+1][y-m2+i+1]
-        j+=1
-    grid[x-m2+j][y-m2+j] = first
-
-    # 방향 3
-    # r+1,c-1
-    x,y = x-m2,y-m2
-    third = grid[x+m3][y-m3]
-    j = 0
-    for i in range(m3-1):
-        grid[x+m3-i][y-m3+i] = grid[x+m3-i-1][y-m3+i+1]
-        j+=1
-    grid[x+m3-j][y-m3+j] = second
-
-    # 방향 4
-    # r+1, c+1
-    x,y = x+m3,y-m3
-    four = grid[x+m4][y+m4]
-    j = 0
-    for i in range(m4-1):
-        grid[x+m4-i][y+m4-i] = grid[x+m4-i-1][y+m4-i-1]
-        j+=1
-    grid[x+m4-j][y+m4-j] = third
-
-else:
-    # 방향 1
-    # r+1, c-1s
-    first = grid[r][c]
-    i = 0
-    for i in range(m1):
-        grid[r-i][c+i] = grid[r-i-1][c+i+1]
-
-    # 방향 2
-    # r+1,c+1
-    x,y = r-m1,c+m1
-    second = grid[x-m2][y-m2]
-    j = 0
-    for i in range(m2-1):
-        grid[x-i][y-i] = grid[x-i-1][y-i-1]
-        j+=1
-    grid[x-j][y-j] = second
-
-    # 방향 3
-    # r-1,c+1
-    x,y = x-m2,y-m2
-    third = grid[x+m3][y-m3]
-    j = 0
-    for i in range(m3):
-        grid[x+i][y-i] = grid[x+i+1][y-i-1]
-
-    # 방향 4
-    # r-1, c-1
-    x,y = x+m3,y-m3
-    j = 0
-    for i in range(m4-1):
-        grid[x+i][y+i] = grid[x+i+1][y+i+1]
-        j+=1
-    grid[x+j][y+j] = first
+def shift(x, y, k, l, move_dir):
+    if move_dir == CCW:
+        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+        move_nums = [k, l, k, l]
+    else:
+        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        move_nums = [l, k, l, k]
     
+    # Step1. temp 배열에 grid 값을 복사합니다.
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+
+    # Step2. 기울어진 직사각형의 경계를 쭉 따라가면서
+    #        숫자를 한 칸씩 밀었을 때의 결과를
+    #        temp에 저장합니다.
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            nx, ny = x + dx, y + dy
+            temp[nx][ny] = grid[x][y]
+            x, y = nx, ny
     
-for row in grid:
-    print(*row)
+    # Step3. temp값을 grid에 옮겨줍니다.
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
+
+
+
+x, y, m1, m2, m3, m4, d = tuple(map(int, input().split()))
+shift(x - 1, y - 1, m1, m2, d)
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
