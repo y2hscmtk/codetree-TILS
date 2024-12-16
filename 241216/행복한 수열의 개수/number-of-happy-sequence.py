fbb2bc398ae822ddb4372fb41428dd1d70ea50a1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-16 22:03:11 +0900

    Title: 행복한 수열의 개수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241216/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,36 @@
+n, m = map(int,input().split())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+result = 0
+
+# 열에 대해서
+for i in range(n):
+    last = graph[i][0]
+    count = 1
+    for j in range(1,n):
+        if graph[i][j] == last:
+            count+=1
+        else: # 다를 경우
+            last = graph[i][j]
+            count = 1
+        if count == m: # 목표한 수만큼 찾았으면 해당 방향에 대해서는 탐색 필요 없음
+            result += 1
+            break
+
+# 행에 대해서
+for i in range(n):
+    last = graph[0][i]
+    count = 1
+    for j in range(1,n):
+        if graph[j][i] == last:
+            count+=1
+        else: # 다를 경우
+            last = graph[j][i]
+            count = 1
+        if count == m: # 목표한 수만큼 찾았으면 해당 방향에 대해서는 탐색 필요 없음
+            result += 1
+            break
+
+print(result)
+        
